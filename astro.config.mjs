a154b0fd58c01e5aa68184afbcd4d26023757a4e
Marlon <marlon.service@protonmail.com>
2026-05-03 21:49:05 +0200

    dotted bg added and new glossy icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,3 @@
 import { defineConfig } from 'astro/config';
-import { z } from 'astro:content'; // <-- Add this line
 
-export default defineConfig({
-  collections: {
-    projects: {
-      type: 'content',
-      schema: () => ({
-        title: z.string(),
-        date: z.date(),
-        description: z.string().optional(),
-      }),
-    },
-  },
-});
+export default defineConfig({});
